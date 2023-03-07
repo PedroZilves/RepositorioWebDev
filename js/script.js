7fc5ee91468c4751d77b6e17dd19734a7b16810c
PedroZilves <RM99482@fiap.com.br>
2023-03-07 11:49:01 -0300

    script.js adicionado ao html

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Este é um comentario de linha 
+
+
+/**
+ * Comentario
+ * de
+ * bloco
+ */ 
+//exemplo das funçoes do console
+console.log("Ola mundo ")
+console.error("Deu zika")
+console.warn("Será?")
+console.info("Vai ser...")
